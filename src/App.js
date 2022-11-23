399482937afefb7817ca90af55b819eeaf7a4bbe
Racyn Ibaya <racynibaya@outlook.com>
2022-11-22 23:07:27 -0500

    adding all the assets and setting general classes

--- src/App.js
+++ src/App.js
@@ -10,10 +10,25 @@ import {
 } from './containers';
 
 import { CTA, Brand, Navbar } from './components';
+
 import './App.css';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
